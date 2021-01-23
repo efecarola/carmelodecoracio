65096d6f7105f1eb59ab06c87533417e1c7c02d8
efecarola <carolafleita@gmail.com>
2021-01-23 14:55:34 -0300

    nueva tienda

---
+++ pages/data.js
@@ -0,0 +1,35 @@
+const products = [
+    {
+        "id" : "AA00",
+        "name" : "Sandwich",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/data4.jpg",
+        "price" : 70,
+        "available": true,
+        tags: ["clasico", "tanto gusto", "bread"]
+    },{
+        "id" : "BB01",
+        "name" : "Bagel",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/data2.jpg",
+        "price" : 80,
+        "available": true,
+        tags: ["clasico", "tanto gusto", "bread"]
+    },{
+        "id" : "CC02",
+        "name" : "Wraps",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/data1.jpg",
+        "price" : 60,
+        "available": true,
+        tags: ["clasico", "tanto gusto", "bread"]
+    },{
+        "id" : "DD03",
+        "name" : "Muffins Salados",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/data3.jpg",
+        "price" : 50,
+        "available": true,
+        tags: ["clasico", "tanto gusto", "bread"]
+    }
+];
