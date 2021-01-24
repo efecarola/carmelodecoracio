6f23982383393e3f687554833d1c024313ca65fa
efecarola <carolafleita@gmail.com>
2021-01-23 22:40:31 -0300

    tienda

---
+++ pages/DomBuilder.js
@@ -0,0 +1,39 @@
+
+// Esta clase va a permitir crear elementos en el HTML sin especificacion
+
+class DOMBuilder{
+    constructor(){
+
+    }
+    h2(title){
+        const h2 = document.createElement('h2');
+        h2.textContent = title;
+        return h2;
+    }
+
+    img(src) {
+        const img = document.createElement('img');
+        img.src = src;
+
+        return img;
+    }
+
+    p(content){
+        const p = document.createElement('p');
+        p.textContent = content;
+
+        return p;
+    }
+
+    // El parametro id es para identificar el boton y el producto que selecciono
+    button(content, className, id){
+        const button = document.createElement('button');
+        button.textContent = content;
+    // Class list es lo que tengo que usar para darle los estilos al html, creando la clase en css
+        button.classList.add(className);
+        // Metodo que permite agregar atributo al boton
+        button.setAttribute('data-id', id);
+
+        return button;
+    }
+}
--- pages/data.js
+++ pages/data.js
@@ -1,35 +1,99 @@
 const products = [
     {
-        "id" : "AA00",
-        "name" : "Sandwich",
+        "id" : "Natural00",
+        "name" : "Duo Flores",
         "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
-        "img" : "../img/data4.jpg",
-        "price" : 70,
+        "img" : "../img/producto1.jpg",
+        "price" : 2200,
         "available": true,
-        tags: ["clasico", "tanto gusto", "bread"]
+        tags: ["cuadros", "decorativos", "diseño"]
     },{
-        "id" : "BB01",
-        "name" : "Bagel",
+        "id" : "Natural01",
+        "name" : "Duo Inhale y Exhale",
         "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
-        "img" : "../img/data2.jpg",
-        "price" : 80,
+        "img" : "../img/producto2.jpg",
+        "price" : 2200,
         "available": true,
-        tags: ["clasico", "tanto gusto", "bread"]
+        tags: ["cuadros", "decorativos", "diseño"]
     },{
-        "id" : "CC02",
-        "name" : "Wraps",
+        "id" : "Natural02",
+        "name" : "Triptico Dulce Hogar",
         "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
-        "img" : "../img/data1.jpg",
-        "price" : 60,
+        "img" : "../img/producto3.jpg",
+        "price" : 2400,
         "available": true,
-        tags: ["clasico", "tanto gusto", "bread"]
+        tags: ["cuadros", "decorativos", "diseño"]
     },{
-        "id" : "DD03",
-        "name" : "Muffins Salados",
+        "id" : "Esley01",
+        "name" : "Duo grita",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto4.jpg",
+        "price" : 1600,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Esley02",
+        "name" : "Es Ley",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto5.jpg",
+        "price" : 800,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Esley03",
+        "name" : "Es ley violeta",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto6.jpg",
+        "price" : 800,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Infantil00",
+        "name" : "Duo Abecedario",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto7.jpg",
+        "price" : 1600,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Infantil01",
+        "name" : "Duo niñas",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto9.jpg",
+        "price" : 2400,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Infantil02",
+        "name" : "Personalizado Nombre",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto10.jpg",
+        "price" : 800,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Apasionados00",
+        "name" : "Brisha",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto11.jpg",
+        "price" : 1100,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Apasionados01",
+        "name" : "Amorpuerta",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto12.jpg",
+        "price" : 800,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Apasionados02",
+        "name" : "SiempreenLibertad",
         "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
         "img" : "../img/data3.jpg",
-        "price" : 50,
+        "price" : 1100,
         "available": true,
-        tags: ["clasico", "tanto gusto", "bread"]
+        tags: ["cuadros", "decorativos", "diseño"]
     }
 ];
