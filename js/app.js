6acd9b2a6dbea7a46a80254c0f18964a1f13d6d7
efecarola <carolafleita@gmail.com>
2021-01-26 14:56:10 -0300

    arreglo de archivos

---
+++ js/app.js
@@ -0,0 +1,85 @@
+// Funcion para crear un card manipulando el DOM
+function buildProductCard(product){
+    // DIV PADRE 
+    const div = document.createElement('div');
+    
+    const title = domBuilder.h2(product.name);
+    const image = domBuilder.img(product.img);
+    const description = domBuilder.p(product.description);
+    const price = domBuilder.p(product.price);
+    const button = domBuilder.button('Seleccionar','btnProduct', product.id);
+    
+
+
+    // Aca incluyo los elementos al contenedor (html) DIV 
+    div.appendChild(title);
+    div.appendChild(image);
+    div.appendChild(description);
+    div.appendChild(price);
+    div.appendChild(button);
+    
+    return div;
+}
+
+// Funcion para el boton cuando hace click
+function onSelectClick(event) {
+    // Con esto me devuelve el ID del boton cuando hago click
+    const idProduct = event.target.dataset.id;
+    // Con esto tomo el objeto del data que coincida con el id seleccionado
+    selectedProduct = products.find(function(product){
+        if (product.id == idProduct){
+            return product;
+        }
+    });
+    
+    // Cada vez que selecciona pushea en el array vacio el producto
+    selectedProducts.push(selectedProduct);   
+    
+    // Llamo a la funcion de crear la card del producto select
+    buildCart();
+}
+
+// Funcion para generar una card de los productos seleccionados
+
+function buildCart(){
+    // Cada vez que selecciono un producto voy a obtener un card con los seleccionados
+    const lastProduct = selectedProducts[selectedProducts.length -1];
+    const card = buildProductCard(lastProduct);
+    selectedContainer.appendChild(card);
+
+    totalProductsContainer.textContent = selectedProducts.length;
+}
+
+
+// Instancio la clase (objeto) que cree en DomBuilder.js
+const domBuilder = new DOMBuilder();
+// Este array se va a llenar con las selecciones que se hagan
+let selectedProducts = [];
+
+// Funcion para que el codigo para que la app inicie cuando el documentento esta cargado
+window.addEventListener('load', function(){
+    // Aca defino que quiero que se creen en el div contenedor que tengo hardcodeado en el html    
+    const productContainer = document.getElementById('productContainer');
+    const selectedContainer = document.getElementById('selectedContainer');
+    const totalProductsContainer = document.getElementById('totalProducts');
+// Foreach para recorrer el arry de products y generar el html dinamicamente   
+    products.forEach(function(product){
+    // Hago que la tarjeta se contruya solo si el producto esta disponible
+        if(product.available){
+            // Llamo a la funcion builProductCard par crear la card en cada vuelta
+            const card = buildProductCard(product);
+            // Al productContainer le agrego un hijo (las card que cree)
+            productContainer.appendChild(card);
+        }
+    });
+
+// EN ESTE PUNTO TENGO EL DOM LISTO
+// Ahora voy a acceder a los botones
+// Con querySlectorAll llamo a todos los elementos que tengan una misma clase o id
+    const btnProducts = document.querySelectorAll('.btnProduct');
+    // hago un forEach para recorrer todos los botones
+    btnProducts.forEach(function(btnProduct){
+        // evento para que escuche el click
+        btnProduct.addEventListener('click', onSelectClick );
+    })
+})
---
+++ js/data.js
@@ -0,0 +1,99 @@
+const products = [
+    {
+        "id" : "Natural00",
+        "name" : "Duo Flores",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto1.jpg",
+        "price" : 2200,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Natural01",
+        "name" : "Duo Inhale y Exhale",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto2.jpg",
+        "price" : 2200,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Natural02",
+        "name" : "Triptico Dulce Hogar",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto3.jpg",
+        "price" : 2400,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Esley01",
+        "name" : "Duo grita",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto4.jpg",
+        "price" : 1600,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Esley02",
+        "name" : "Es Ley",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto5.jpg",
+        "price" : 800,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Esley03",
+        "name" : "Es ley violeta",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto6.jpg",
+        "price" : 800,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Infantil00",
+        "name" : "Duo Abecedario",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto7.jpg",
+        "price" : 1600,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Infantil01",
+        "name" : "Duo niñas",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto9.jpg",
+        "price" : 2400,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Infantil02",
+        "name" : "Personalizado Nombre",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto10.jpg",
+        "price" : 800,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Apasionados00",
+        "name" : "Brisha",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto11.jpg",
+        "price" : 1100,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Apasionados01",
+        "name" : "Amorpuerta",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/producto12.jpg",
+        "price" : 800,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    },{
+        "id" : "Apasionados02",
+        "name" : "SiempreenLibertad",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora porro doloremque consectetur nam cum nulla illo et, possimus facere sapiente ipsam quasi, inventore totam harum odit tempore animi facilis.",
+        "img" : "../img/data3.jpg",
+        "price" : 1100,
+        "available": true,
+        tags: ["cuadros", "decorativos", "diseño"]
+    }
+];
